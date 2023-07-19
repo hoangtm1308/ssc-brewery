bd07b87b82328f885026f42d6e5d4781a8ecb05d
hoangtran <tranminhhoang@luvina.net>
2023-07-19 17:18:09 +0700

    Custom Password Encoder implementation using DelegatingPassword Encoder with different Encoder type, Unit test for password encoder testing

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -3,8 +3,6 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,25 +25,25 @@ public class BeerControllerIT extends BaseIT {
     @Test
     void findBeersWithHttpBasic() throws Exception {
         mockMvc.perform(get("/beers/find")
-                        .with(httpBasic("hoangtm", "1308n5ggp")))
+                        .with(httpBasic("hoangtm", "hoangtm"))) // đang được encode theo LDAP nên case này fail
                 .andExpect(status().isOk()).andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
     }
 
     //test case fail do đã config trong SecurityConfig
-    @Test
-    void findBeersWithAnonymous() throws Exception {
-        mockMvc.perform(get("/beers/find")
-                        .with(anonymous()))
-                .andExpect(status().isOk()).andExpect(view().name("beers/findBeers"))
-                .andExpect(model().attributeExists("beer"));
-    }
+//    @Test
+//    void findBeersWithAnonymous() throws Exception {
+//        mockMvc.perform(get("/beers/find")
+//                        .with(anonymous()))
+//                .andExpect(status().isOk()).andExpect(view().name("beers/findBeers"))
+//                .andExpect(model().attributeExists("beer"));
+//    }
 
     //Test case to test that In Memory User Details Entity is existed
     @Test
     void createNewBeersUsingUserDetails() throws Exception {
         mockMvc.perform(get("/beers/new")
-                        .with(httpBasic("user", "password")))
+                        .with(httpBasic("user", "hoangtm")))
                 .andExpect(status().isOk()).andExpect(view().name("beers/createBeer"))
                 .andExpect(model().attributeExists("beer"));
     }
--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,8 +1,16 @@
 package guru.sfg.brewery.web.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.util.DigestUtils;
+
+import static org.junit.Assert.assertTrue;
 
 @Slf4j
 public class PasswordEncodingTest {
@@ -10,12 +18,57 @@ public class PasswordEncodingTest {
     static final String PASSWORD = "hoangtm";
 
     @Test
-    void hashingExample(){
+    void hashingExampleWithMD5(){
         //using MD5 to hash value
+        //Spring Security giữ để support những hệ thống cổ
         log.info(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
 
         //Adding hash value to password and hashing using MD5 encoding
         String salted = PASSWORD + "ThisIsMySALTVALUE";
         log.info(DigestUtils.md5DigestAsHex(salted.getBytes()));
+    }
+
+    @Test
+    void testNoOpEncoding(){
+        //Spring Security giữ để support những hệ thống cổ
+        PasswordEncoder noOp = NoOpPasswordEncoder.getInstance();
+        log.info(noOp.encode(PASSWORD));
+    }
+
+    @Test
+    void testLDAPEncoding(){
+        PasswordEncoder ldap = new LdapShaPasswordEncoder();
+
+        log.info(ldap.encode(PASSWORD));
+        log.info(ldap.encode(PASSWORD));
+
+        String encodePwd = ldap.encode(PASSWORD);
+
+        Assertions.assertTrue(ldap.matches(PASSWORD,encodePwd));
+    }
+
+    @Test
+    void testSha256(){
+        PasswordEncoder sha256 = new StandardPasswordEncoder();
+
+        log.info(sha256.encode(PASSWORD));
+        log.info(sha256.encode(PASSWORD));
+    }
+
+    @Test
+    void testBcrypt(){
+        PasswordEncoder bcrypt = new BCryptPasswordEncoder();
+
+        log.info(bcrypt.encode(PASSWORD));
+        log.info(bcrypt.encode(PASSWORD));
+    }
+
+    @Test
+    void testBcrypt15(){
+        PasswordEncoder bcrypt = new BCryptPasswordEncoder(15);
+
+        log.info(bcrypt.encode(PASSWORD));
+        log.info(bcrypt.encode(PASSWORD));
+        log.info(bcrypt.encode("tiger"));
     }
 }
