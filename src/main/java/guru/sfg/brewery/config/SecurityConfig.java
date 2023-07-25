30b68807cb62907e6903f19d4459cf58f18b8cb2
hoangtran <tranminhhoang@luvina.net>
2023-07-25 16:47:25 +0700

    Convert JPA User to Spring Security User and add role and authorize for specific url, remove unnecessary filter

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,53 +1,44 @@
 package guru.sfg.brewery.config;
 
 import guru.sfg.brewery.security.CustomPasswordEncoderFactories;
-import guru.sfg.brewery.security.RestHeaderAuthFilter;
-import guru.sfg.brewery.security.RestUrlAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //Method dùng để setup và cấu trúc filter những rest header
-    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
-        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
+//    @Autowired
+//    JpaUserDetailsService jpaUserDetailsService;
 
-    public RestUrlAuthFilter restUrlAuthFilter(AuthenticationManager authenticationManager){
-        RestUrlAuthFilter filter = new RestUrlAuthFilter(new AntPathRequestMatcher("/api/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
+    //Method dùng để setup và cấu trúc filter những rest header
+//    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
+//        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+//        filter.setAuthenticationManager(authenticationManager);
+//        return filter;
+//    }
+//
+//    public RestUrlAuthFilter restUrlAuthFilter(AuthenticationManager authenticationManager){
+//        RestUrlAuthFilter filter = new RestUrlAuthFilter(new AntPathRequestMatcher("/api/**"));
+//        filter.setAuthenticationManager(authenticationManager);
+//        return filter;
+//    }
 
     //Override các config trong WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         //Config Spring Security để sử dụng filter ở trên
-        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-                UsernamePasswordAuthenticationFilter.class)
-                .csrf().disable();
-
-        http.addFilterBefore(restUrlAuthFilter(authenticationManager()),UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
+//                UsernamePasswordAuthenticationFilter.class)
+//                .csrf().disable();
+//
+//        http.addFilterBefore(restUrlAuthFilter(authenticationManager()),UsernamePasswordAuthenticationFilter.class);
 
         // "/*" cho phép các tầng truy cập cùng level với dấu sao
         // "/**" cho phép toàn bộ sau dấu /
@@ -56,8 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     authorize
                             .antMatchers("/h2-console/**").permitAll()
                             .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-//                        .antMatchers("/beers/find","/beer*").permitAll() //config thêm find beer không cần đăng nhập
+                            .antMatchers("/beers/find","/beer*").permitAll() //config thêm find beer không cần đăng nhập
                             .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                            .mvcMatchers(HttpMethod.DELETE,"api/v1/beer/**").hasRole("ADMIN")
+                            .mvcMatchers(HttpMethod.GET,"/brewery/breweries","/brewery/api/v1/breweries").hasRole("CUSTOMER")
+//                            .mvcMatchers(HttpMethod.GET,"/brewery/api/v1/breweries").hasRole("CUSTOMER")
                             .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
 
                 })
@@ -65,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().and()
+                .csrf().disable()
                 .httpBasic();
 
         //h2 console config
@@ -91,7 +86,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
 
     @Bean
-        //Config NoOpPasswordEncoder
     PasswordEncoder passwordEncoder() {
         //Spring Security giữ để support những hệ thống cổ
         //return NoOpPasswordEncoder.getInstance();
@@ -102,23 +96,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return CustomPasswordEncoderFactories.customDelegatingPasswordEncoder();
     }
 
-    //Create User doing Authentication Fluent API (Authentication Manager Builder) - Difference way
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("hoangtm")
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        Using JPA User to create the Spring Security User
+//        auth.userDetailsService(this.jpaUserDetailsService).passwordEncoder(passwordEncoder());
+
+        //Create User doing Authentication Fluent API (Authentication Manager Builder) - Difference way
+//        auth.inMemoryAuthentication()
+//                .withUser("hoangtm")
 //                .password("{noop}1308n5ggp") // dùng {noop} cho những password cần encode
-                .password("{ldap}{SSHA}BBhtzKbfFCDcTFuUotq3Qd0jxLulMqzNEDaTaw==")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
+//                .password("{ldap}{SSHA}BBhtzKbfFCDcTFuUotq3Qd0jxLulMqzNEDaTaw==")
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("user")
 //                .password("{noop}password")  // dùng {noop} cho những password cần encode
-                .password("{bcrypt}$2a$10$sUAPRNUFBCK7C4pbx2aNbOMWrHGTD6tn61abtE633yJ9sDe.rviem")
-                .roles("user")
-                .and()
-                .withUser("scott")
+//                .password("{bcrypt}$2a$10$sUAPRNUFBCK7C4pbx2aNbOMWrHGTD6tn61abtE633yJ9sDe.rviem")
+//                .roles("user")
+//                .and()
+//                .withUser("scott")
 //                .password("{noop}tiger")
-                .password("{bcrypt15}$2a$15$68GpwChurZgcS3.rd8FVU.95n2/NTXGJz8PZ1Z08O64cCSt0tygHK")
-                .roles("CUSTOMER");
-    }
+//                .password("{bcrypt15}$2a$15$68GpwChurZgcS3.rd8FVU.95n2/NTXGJz8PZ1Z08O64cCSt0tygHK")
+//                .roles("CUSTOMER");
+//    }
 }
--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -4,13 +4,16 @@ import guru.sfg.brewery.domain.security.Authority;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.beans.Transient;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,12 +21,17 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class JpaUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        log.debug("--------------- Getting user info via JPA ---------------------------");
+
         User user = userRepository.findByUsername(username).orElseThrow(() -> {
             return new UsernameNotFoundException("User name" + username +"not found");
         });
@@ -38,10 +46,11 @@ public class JpaUserDetailsService implements UserDetailsService {
 
     private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
         if(authorities != null && authorities.size() > 0){
-            return authorities.stream()
-                    .map(Authority::getRole)
-                    .map(SimpleGrantedAuthority::new)
+            Set<GrantedAuthority> grantedAuthoritySet = authorities.stream()
+                    .map(authority -> authority.getRole()) //Can use Method Reference
+                    .map(role -> new SimpleGrantedAuthority(role)) //Can use Method Reference
                     .collect(Collectors.toSet());
+            return grantedAuthoritySet;
         }else {
             return new HashSet<>();
         }
