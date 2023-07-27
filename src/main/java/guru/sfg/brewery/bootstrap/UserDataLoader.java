05335918d546f249869ff1ee2b89aaab6f6ba308
hoangtran <tranminhhoang@luvina.net>
2023-07-27 14:50:45 +0700

    Refactoring Data Loader base on new construction of Entity for more specification

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -1,8 +1,10 @@
 package guru.sfg.brewery.bootstrap;
 
 import guru.sfg.brewery.domain.security.Authority;
+import guru.sfg.brewery.domain.security.Role;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.AuthorityRepository;
+import guru.sfg.brewery.repositories.security.RoleRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +12,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Set;
+
 @RequiredArgsConstructor
 @Component
 @Slf4j
 public class UserDataLoader implements CommandLineRunner {
 
     private final AuthorityRepository authorityRepository;
+    private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
 
@@ -28,26 +34,46 @@ public class UserDataLoader implements CommandLineRunner {
 
     private void loadSecurityData(){
 
-        Authority admin = authorityRepository.save(Authority.builder().role("ROLE_ADMIN").build());
-        Authority userRole = authorityRepository.save(Authority.builder().role("ROLE_USER").build());
-        Authority customer = authorityRepository.save(Authority.builder().role("ROLE_CUSTOMER").build());
+        //Beer Authorities Setup
+        Authority createBeer = authorityRepository.save(Authority.builder().permission("beer.create").build());
+        Authority updateBeer = authorityRepository.save(Authority.builder().permission("beer.update").build());
+        Authority readBeer = authorityRepository.save(Authority.builder().permission("beer.read").build());
+        Authority deleteBeer = authorityRepository.save(Authority.builder().permission("beer.delete").build());
 
+        //Create new Roles
+        Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
+        Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
+        Role userRole = roleRepository.save(Role.builder().name("USER").build());
+
+        //Authorization for each role
+        adminRole.setAuthorities(Set.of(createBeer,updateBeer,readBeer,deleteBeer));
+        customerRole.setAuthorities(Set.of(readBeer));
+        userRole.setAuthorities(Set.of(readBeer));
+
+        //Save all role to DB
+        roleRepository.saveAll(Arrays.asList(adminRole,customerRole,userRole));
+
+//        Authority admin = authorityRepository.save(Authority.builder().role("ROLE_ADMIN").build());
+//        Authority userRole = authorityRepository.save(Authority.builder().role("ROLE_USER").build());
+//        Authority customer = authorityRepository.save(Authority.builder().role("ROLE_CUSTOMER").build());
+
+        //Save new user with specific role
         userRepository.save(User.builder()
                 .username("hoangtm")
                 .password(encoder.encode("hoangtm"))
-                .authority(admin)
+                .role(adminRole)
                 .build());
 
         userRepository.save(User.builder()
                 .username("user")
                 .password(encoder.encode("hoangtm"))
-                .authority(userRole)
+                .role(userRole)
                 .build());
 
         userRepository.save(User.builder()
                 .username("scott")
                 .password(encoder.encode("hoangtm"))
-                .authority(customer)
+                .role(userRole)
                 .build());
 
         log.debug("User Loaded: {}", userRepository.count());
